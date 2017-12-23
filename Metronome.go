2039e8184333d25677d462820d1fc291a3bf5a28
Gyroscope56 <hi2arvindkumar@gmail.com>
2017-12-23 12:11:38 -0800

    Added a good stop button

--- Metronome.go
+++ Metronome.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("Starting Metronome Server")
 	panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 
 }
